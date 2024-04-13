657d7e1b16d76791874dd1763b4991c6e1427d71
Luis Carlos <luiscarloshurtado@live.com>
2024-04-13 12:04:28 -0500

    Update

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,5 +1,18 @@
+// Init Config
 Go.config("appIcon", Go.base("", "img/icons/256x256.png"));
+Go.config("appLang", "en");
 
-Go.import(Go.base("", "js/views.js"), (module) => {
-  console.log(module);
+// Load views/routes
+Go.import(Go.base("", "js/views.js"), async (views) => {
+  // Get current user language
+  const lang = Go.lang().current() || Go.config("appLang");
+
+  // Load translations
+  await Go.import(Go.base("", "js/lang/" + lang + ".js"));
+
+  // Load views/routes
+  Go.views(views);
+
+  // Start nav/router
+  Go.do("nav/start");
 });
