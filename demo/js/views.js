bdf16631d705722c78a688b96f76ed797af3b3f4
Luis Carlos <luiscarloshurtado@live.com>
2024-04-13 12:45:09 -0500

    Update

--- demo/js/views.js
+++ demo/js/views.js
@@ -4,6 +4,9 @@ export default {
     title: `${Go.lang("docs")} - ${Go.lang("app_name")}`,
     components: [{ target: ".app", src: Go.base("", "html/home.html") }],
   },
+  "/go.sdk/demo/": () => {
+    return Go.do("nav/getRouteByName", "home");
+  },
   404: {
     name: "404",
     title: `${Go.lang("404")} - ${Go.lang("app_name")}`,
