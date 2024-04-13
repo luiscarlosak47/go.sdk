9f98459178fab4c32f4ce5950353b76d63ee6c18
Luis Carlos <luiscarloshurtado@live.com>
2024-04-13 09:39:54 -0500

    Update

---
+++ demo/js/views.js
@@ -0,0 +1,7 @@
+export default {
+  "/": {
+    name: "home",
+    title: `${Go.lang("docs")} - ${Go.lang("app_name")}`,
+    components: [{ target: ".body", src: Go.base("", "html/home.html") }],
+  },
+};
